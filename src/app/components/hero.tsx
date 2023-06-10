5075339efea04bbc7419172313aa7ed9871a74a6
Eleana Cano <99444615+EleanaCano@users.noreply.github.com>
2023-06-10 20:28:33 +0200

    Airtable1

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,7 +8,7 @@ import {
 const Hero = () => {
     return (
         <div className='w-full h-screen bg-zinc-200 flex flex-col justify-between'>
-            <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+            <div className='grid md:grid-cols-2 max-w-full m-auto'>
                 <div className='flex flex-col justify-center md:items-start w-full px-2 py-8 text-black'>
                     <h1 className='py-3 text-5xl md:text-7xl font-bold'>Love, Death & Robots</h1>
                     <p className='py-3 px-1 text-2xl'>Is a surreal sci-fi-horror-comedy anthology</p>
---
+++ src/app/episodes/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import { Header } from '../components/header';
+
+export default function About() {
+    return (
+        <main className="mt-6">
+            <Header>Episodes</Header>
+      
+            <p className="mt-5">To read something more.</p>
+        </main>
+    );
+}
